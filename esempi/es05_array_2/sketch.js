12f46866f6fa745bbcebab18d515336eecbdf0c9
SofiaRibeiroMarques <sofiarm05@hotmail.com>
2026-03-17 15:58:56 +0100

    esempi

--- esempi/es05_array_2/sketch.js
+++ esempi/es05_array_2/sketch.js
@@ -1,32 +1,39 @@
-const numero = 1000
 const formiche =[]
+let mappa
+let dim = 10
 
+function preload(){
+    mappa = loadImage("img/shrek-scaled.jpg")
+
+
+
+}
 
 function setup(){
     createCanvas(500, 500)
         background(200)
-
-    for(let i = 0; i <numero; i++){
-    formiche[i] ={
-        x : width/2,
-        y : height/2,
-        colore : color(random(255), random(255), random(255))  
-         }
+for(let j = 0; j <mappa.height; j++){
+    for(let i = 0; i <mappa.width; i++){
+    formiche.push({
+        x : i * dim,
+        y : j * dim,
+        colore : mappa.get(i, j), 
+        dim : dim
+         })
     }
-
+}
 
 }
 
 function draw(){
     noStroke()
-for(let i = 0; i <numero; i++){
+for(let i = 0; i <formiche.length; i++){
     fill(formiche[i].colore)
     rect(formiche[i].x, formiche[i].y, 4, 4)
 
- formiche[i].x += random(-4, 4)
-  formiche[i].y += random(-4, 4)
+ formiche[i].x += random(-1, 1)
+  formiche[i].y += random(-1, 1)
 
 
 }
 }
-
